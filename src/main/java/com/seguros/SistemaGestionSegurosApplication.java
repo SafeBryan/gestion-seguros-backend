8f3ef9b7611c2d551714b396be672288b14c64a7
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-04-08 10:24:54 -0500

    Primer Commit

---
+++ src/main/java/com/seguros/SistemaGestionSegurosApplication.java
@@ -0,0 +1,13 @@
+package com.seguros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionSegurosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionSegurosApplication.class, args);
+	}
+
+}
