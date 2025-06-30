b8db4a633afc52f9de3079c175de7a41e43dd725
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-06-30 10:52:25 -0500

    Configurar clase principal para despliegue en Tomcat con WAR

--- src/main/java/com/seguros/SistemaGestionSegurosApplication.java
+++ src/main/java/com/seguros/SistemaGestionSegurosApplication.java
@@ -2,12 +2,19 @@ package com.seguros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SistemaGestionSegurosApplication {
+public class SistemaGestionSegurosApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaGestionSegurosApplication.class, args);
 	}
 
-} 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SistemaGestionSegurosApplication.class);
+	}
+}
+
