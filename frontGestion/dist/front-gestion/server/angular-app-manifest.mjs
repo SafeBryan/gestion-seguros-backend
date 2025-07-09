c464726fa58382e35d033bbf3f0166981a76e5f5
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-07-09 14:20:21 -0500

    Se agrego U

--- frontGestion/dist/front-gestion/server/angular-app-manifest.mjs
+++ frontGestion/dist/front-gestion/server/angular-app-manifest.mjs
@@ -57,7 +57,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-W3Q74Q4Z.js",
+      "chunk-5EDD4X6S.js",
       "chunk-QMQTSFHS.js",
       "chunk-FK6H3RFT.js"
     ],
@@ -67,7 +67,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-W3Q74Q4Z.js",
+      "chunk-5EDD4X6S.js",
       "chunk-QMQTSFHS.js",
       "chunk-FK6H3RFT.js"
     ],
@@ -76,7 +76,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-W3Q74Q4Z.js",
+      "chunk-5EDD4X6S.js",
       "chunk-QMQTSFHS.js",
       "chunk-FK6H3RFT.js"
     ],
@@ -85,7 +85,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-W3Q74Q4Z.js",
+      "chunk-5EDD4X6S.js",
       "chunk-QMQTSFHS.js",
       "chunk-FK6H3RFT.js"
     ],
@@ -94,7 +94,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-W3Q74Q4Z.js",
+      "chunk-5EDD4X6S.js",
       "chunk-QMQTSFHS.js",
       "chunk-FK6H3RFT.js"
     ],
@@ -103,7 +103,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-W3Q74Q4Z.js",
+      "chunk-5EDD4X6S.js",
       "chunk-QMQTSFHS.js",
       "chunk-FK6H3RFT.js"
     ],
@@ -112,7 +112,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-W3Q74Q4Z.js",
+      "chunk-5EDD4X6S.js",
       "chunk-QMQTSFHS.js",
       "chunk-FK6H3RFT.js"
     ],
@@ -126,25 +126,25 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 28178, hash: 'a6cb03275f11d590d6f5dd71f6b7a15b00d036a8f60038839a5573ef7a7733db', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17245, hash: '7fe33326f36a609be6b62d99d127a9a9993d15e7d549c5472085f0268bab0bad', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'usuarios/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/usuarios_index_html.mjs').then(m => m.default)},
-    'seguros/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/seguros_index_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
-    'contratos/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/contratos_index_html.mjs').then(m => m.default)},
-    'clientes/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/clientes_index_html.mjs').then(m => m.default)},
-    'pagos/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/pagos_index_html.mjs').then(m => m.default)},
-    'depositos/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/depositos_index_html.mjs').then(m => m.default)},
-    'reembolsos/crear/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/reembolsos_crear_index_html.mjs').then(m => m.default)},
-    'reembolsos/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/reembolsos_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'reembolsos/pendientes/index.html': {size: 35925, hash: 'd952c65df1f9c89d1d80f9bb6eba96cfc5b65d89569686d9cd3e034d2469290c', text: () => import('./assets-chunks/reembolsos_pendientes_index_html.mjs').then(m => m.default)},
-    'reportes/seguros-impagos/index.html': {size: 36082, hash: '51675e6298022767058e82dee0cd59e926f117d533131a75b5518cecc4408314', text: () => import('./assets-chunks/reportes_seguros-impagos_index_html.mjs').then(m => m.default)},
-    'reportes/dashboard-reportes/index.html': {size: 36082, hash: '51675e6298022767058e82dee0cd59e926f117d533131a75b5518cecc4408314', text: () => import('./assets-chunks/reportes_dashboard-reportes_index_html.mjs').then(m => m.default)},
-    'reportes/contratos-por-cliente/index.html': {size: 36082, hash: '51675e6298022767058e82dee0cd59e926f117d533131a75b5518cecc4408314', text: () => import('./assets-chunks/reportes_contratos-por-cliente_index_html.mjs').then(m => m.default)},
-    'reportes/contratos-vencidos/index.html': {size: 36082, hash: '51675e6298022767058e82dee0cd59e926f117d533131a75b5518cecc4408314', text: () => import('./assets-chunks/reportes_contratos-vencidos_index_html.mjs').then(m => m.default)},
-    'reportes/contratos-por-vencer/index.html': {size: 36082, hash: '51675e6298022767058e82dee0cd59e926f117d533131a75b5518cecc4408314', text: () => import('./assets-chunks/reportes_contratos-por-vencer_index_html.mjs').then(m => m.default)},
-    'reportes/reembolsos-pendientes/index.html': {size: 36082, hash: '51675e6298022767058e82dee0cd59e926f117d533131a75b5518cecc4408314', text: () => import('./assets-chunks/reportes_reembolsos-pendientes_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 28178, hash: '9a3a10822c25a49a5d330a835d8eddfd1ea7d87a48729e67a972cbfa7ad41fba', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17245, hash: '167b60929ec5d35bd5ee3bc88d25b4401093e6320df387c51985ae21f94e8c3c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'seguros/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/seguros_index_html.mjs').then(m => m.default)},
+    'contratos/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/contratos_index_html.mjs').then(m => m.default)},
+    'clientes/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/clientes_index_html.mjs').then(m => m.default)},
+    'pagos/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/pagos_index_html.mjs').then(m => m.default)},
+    'usuarios/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/usuarios_index_html.mjs').then(m => m.default)},
+    'reembolsos/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/reembolsos_index_html.mjs').then(m => m.default)},
+    'depositos/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/depositos_index_html.mjs').then(m => m.default)},
+    'reembolsos/crear/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/reembolsos_crear_index_html.mjs').then(m => m.default)},
+    'reembolsos/pendientes/index.html': {size: 35925, hash: 'efe5d51b87626cc6addd7a7a670cbd027a601452f914f1d807de3834a14f755e', text: () => import('./assets-chunks/reembolsos_pendientes_index_html.mjs').then(m => m.default)},
+    'reportes/contratos-por-cliente/index.html': {size: 36082, hash: 'af16df9facf1e6f602ba6a8a91518f378dff8c3b5d7a532b05957c3178a01826', text: () => import('./assets-chunks/reportes_contratos-por-cliente_index_html.mjs').then(m => m.default)},
+    'reportes/dashboard-reportes/index.html': {size: 36082, hash: 'af16df9facf1e6f602ba6a8a91518f378dff8c3b5d7a532b05957c3178a01826', text: () => import('./assets-chunks/reportes_dashboard-reportes_index_html.mjs').then(m => m.default)},
+    'reportes/seguros-impagos/index.html': {size: 36082, hash: 'af16df9facf1e6f602ba6a8a91518f378dff8c3b5d7a532b05957c3178a01826', text: () => import('./assets-chunks/reportes_seguros-impagos_index_html.mjs').then(m => m.default)},
+    'reportes/reembolsos-pendientes/index.html': {size: 36082, hash: 'af16df9facf1e6f602ba6a8a91518f378dff8c3b5d7a532b05957c3178a01826', text: () => import('./assets-chunks/reportes_reembolsos-pendientes_index_html.mjs').then(m => m.default)},
+    'reportes/contratos-por-vencer/index.html': {size: 36082, hash: 'af16df9facf1e6f602ba6a8a91518f378dff8c3b5d7a532b05957c3178a01826', text: () => import('./assets-chunks/reportes_contratos-por-vencer_index_html.mjs').then(m => m.default)},
+    'reportes/contratos-vencidos/index.html': {size: 36082, hash: 'af16df9facf1e6f602ba6a8a91518f378dff8c3b5d7a532b05957c3178a01826', text: () => import('./assets-chunks/reportes_contratos-vencidos_index_html.mjs').then(m => m.default)},
     'styles-KD5DMBN3.css': {size: 323235, hash: 'NO/6mxV21vM', text: () => import('./assets-chunks/styles-KD5DMBN3_css.mjs').then(m => m.default)}
   },
 };
